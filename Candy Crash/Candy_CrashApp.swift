4af4a39cb8d3c10cc59099d36b01661c5557a865
Anton <danilovanton724@gmail.com>
2024-02-19 17:05:21 +0100

    Initial Commit

---
+++ Candy Crash/Candy_CrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Candy_CrashApp.swift
+//  Candy Crash
+//
+//  Created by Anton on 19/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Candy_CrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Candy Crash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Candy Crash
+//
+//  Created by Anton on 19/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
